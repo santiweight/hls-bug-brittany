fb4842669acc3ce256a56e2b1fcc967c2a357c57
Santi Weight <santiagoweight@forwardnetworks.com>
2020-12-11 18:31:22 -0800

    Bug set up\n To run bug, format Lib.hs; To run normally, uncomment PatSyns extension

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,20 @@
+{-            # LANGUAGE PatternSynonyms #-}
+
 module Lib
-    ( someFunc
+    (                someFunc
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc ::
+       IO
+       ()
+someFunc
+   =
+    putStrLn             "someFunc"
+
+newtype Bar a = Bar a
+
+pattern Foo :: Int
+         -> Bar
+         Int
+pattern Foo a = Bar a
+
